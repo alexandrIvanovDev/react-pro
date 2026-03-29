5ee8e4d9235f5adeda94c4e4d02d350480c8a321
AlexandrIvanov <alexandr.1996@list.ru>
2026-03-29 10:43:57 +0300

    Fix after review

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,7 @@
-import { TaskPage } from 'pages/tasks';
-
 import './styles/index.css';
 import { RegistryForm } from 'features/registryForm/ui/RegistryForm';
 
 function App() {
-  // return <TaskPage />;
   return <RegistryForm />;
 }
 
